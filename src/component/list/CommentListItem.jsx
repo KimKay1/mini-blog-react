94071c46b598f63a13ba8c1bcd582cb29a18f64d
KimKay1 <effloresco@naver.com>
2024-04-06 21:45:13 +0900

    [자습] 240406 reply 수정 기능 (미완성)

--- src/component/list/CommentListItem.jsx
+++ src/component/list/CommentListItem.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
 import styled from "styled-components";
+import Modal from "../ui/Modal";
+import axios from "axios";
+import {v4 as uuidv4} from "uuid";
 
 const Wrapper = styled.div`
     width: calc(100% - 32px);
@@ -24,11 +28,36 @@ const ContentText = styled.p`
 
 function CommentListItem(props) {
     const { comment } = props;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleModalOpen = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleModalClose = () => {
+        setIsModalOpen(false);
+    };
+
+    const editReply = () => {
+        // axios.post(`/reply/write`, {
+        //     content: comment,
+        //     idx: postId // join된 값을 찾아서 처리하기 위해 적어줘야함
+        // })
+        //     .then(response => {
+        //         setUpdateReply(uuidv4()); // 글 올리고 나서 새로 고침하려고 이렇게 해둔건데... 이걸 안 쓰는 방법이 있을까?
+        //         setComment("");
+        //     })
+        //     .catch(error => console.error(error));
+        console.log(props); // props에 idx값은 없고 ridx값은 있음... ridx값으로 idx값 찾아서 수정해야할듯
+    }
 
     return (
-        <Wrapper>
-            <ContentText>{comment.content}</ContentText>
-        </Wrapper>
+        <>
+            <Wrapper onClick={handleModalOpen}> {/* 클릭 시 모달 열기 */}
+                <ContentText>{comment.content}</ContentText>
+            </Wrapper>
+            {isModalOpen && <Modal comment={comment} onClose={handleModalClose} editReply={editReply} />} {/* 모달이 열려있으면 모달 컴포넌트 렌더링 */}
+        </>
     );
 }
 
---
+++ src/component/ui/Modal.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+
+const ModalWrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    //background-color: rgba(0, 0, 0, 0.5); /* 반투명한 배경 */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ModalContent = styled.div`
+    background-color: white;
+    padding: 20px;
+    border-radius: 8px;
+    border: 1px solid;
+`;
+
+// const CloseButton = styled.button`
+//     position: absolute;
+//     top: 10px;
+//     right: 10px;
+//     background: none;
+//     border: none;
+//     cursor: pointer;
+// `;
+
+function Modal({ comment, onClose , editReply }) {
+    const navigate = useNavigate();
+
+    return (
+        <ModalWrapper>
+            <ModalContent onClick={(e) => e.stopPropagation()}>
+                {/*<CloseButton onClick={onClose}>X</CloseButton>*/}
+                <p>{comment.content}</p>
+                <Button
+                    title="수정"
+                    onClick={() => {
+                        editReply();
+                    }}
+                />
+                <Button
+                    title="삭제"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                />
+                <Button
+                    title="닫기"
+                    onClick={onClose}
+                />
+            </ModalContent>
+        </ModalWrapper>
+    );
+}
+
+export default Modal;
